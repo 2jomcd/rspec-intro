8cb9fcd92ad459dd6c7443ddf18fe4e4202cfc09
Dennis Charles Hackethal <dennis@generalassemb.ly>
2015-01-05 20:20:26 -0800

    Add solution

--- lib/car.rb
+++ lib/car.rb
@@ -1,3 +1,23 @@
 class Car
-	
+	attr_accessor :speed
+
+	def initialize
+		self.speed = 0
+	end
+
+	def accelerate(gain)
+		self.speed = [speed + gain, 200].min
+	end
+
+	def self.all
+		ObjectSpace.each_object(self).to_a
+	end
+
+	def decelerate(loss)
+		self.speed = [speed - loss, 0].max
+	end
+
+	def self.race!
+		all.sample
+	end
 end
